d76b25d4cafe4f61b5eeb7bd3ca3ff5155df9998
pedrobenevidesx <pedrohenriquevm215@gmail.com>
2026-08-15 19:49:12 -0300

    feat: add middlewares para rotas

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import http from "node:http"
+
+import { jsonHandler } from "./middlewares/jsonHandler.js"
+import { routeHandler } from "./middlewares/routeHandler.js"
+
+const server = http.createServer(async (req, res) => {
+    await jsonHandler(req, res)
+    
+    routeHandler(req, res)
+})
+
+server.listen(3333, () => {
+    console.log("servidor rodando na porta 3333")
+})
