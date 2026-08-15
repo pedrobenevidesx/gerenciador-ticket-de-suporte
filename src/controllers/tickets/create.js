406ce3b30e2a85ba7e27e16980f4637f97d9d13a
pedrobenevidesx <pedrohenriquevm215@gmail.com>
2026-08-15 20:20:04 -0300

    feat: add controller para criacao de tickets

---
+++ src/controllers/tickets/create.js
@@ -0,0 +1,17 @@
+import { randomUUID } from "node:crypto"
+ 
+export function create({ req, res }) {
+    const { equipamento, descricao, username } = req.body
+
+    const ticket = {
+        id: randomUUID(),
+        equipamento,
+        descricao, 
+        username,
+        status: "open",
+        create_at: new Date(),
+        update_at: new Date()
+    }
+
+    return res.end(JSON.stringify(ticket))
+}
