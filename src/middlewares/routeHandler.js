566f63ab3e4290b817f1027c30d3446066f1958b
Pedro <pedrohenriquevm215@gmail.com>
2026-08-23 23:29:09 -0300

    feat: add filtros por query params

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,14 +1,20 @@
 import { routes } from "../routes/index.js"
 import { Database } from "../database/database.js"
+import { extractQueryParams } from "../utils/extractQueryParams.js"
 
 const database = new Database()
 
 export function routeHandler(req, res) {
     const route = routes.find((route) => {
-        return route.method === req.method && route.path === req.url
+        return route.method === req.method && route.path.test(req.url)
     })
 
     if(route) {
+        const routeParams = req.url.match(route.path)
+
+        const { query } = routeParams.groups
+        req.query = query ? extractQueryParams(query) : {}
+
         return route.controller({ req, res, database })
     }
 
--- src/utils/parseRoutePath.js
+++ src/utils/parseRoutePath.js
@@ -1,9 +1,9 @@
 export function parseRoutePath(path) {
-    const routeParametesRegex = /:([a-zA-Z]+)/g
+    const routeParametersRegex = /:([a-zA-Z]+)/g
 
     const params = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9_]+)")
 
-    const pathRegex = new RegExp(`^${params}(?<querry\\?(.*))?$`)
+    const pathRegex = new RegExp(`^${params}(?<query>\\?(.*))?$`)
 
     return pathRegex
 }
